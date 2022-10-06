b5dcfc3c7e063893074f36f78a21559f6151323f
Dan <daweidern@gmail.com>
2022-10-06 10:58:57 -0500

    small edit to js

--- backend.js
+++ backend.js
@@ -3,21 +3,11 @@ const baseLink = 'http://localhost:3000/animals'
 const main = document.getElementById('main')
 const addAnimal = document.getElementById('submitter')
 
-// Page Load
-document.addEventListener('DOMContentLoaded', () => {
-    return;
-})
-    function pageReload(){
-        main.innerHTML = ''
-    }
-
-    // Event Listeners
+// Event Listeners
 
 addAnimal.addEventListener('click',fetchAnimals)
 
 function fetchAnimals() {
     fetch (baseLink)
     .then (res => res.json)
-    .then (pageReload())
-    
 }
